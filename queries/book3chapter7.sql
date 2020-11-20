e831d62819285fd553c22b9bb300841edcca95a3
David Everett <davideverett1989@gmail.com>
2020-11-19 19:12:38 -0600

    done with chapter 7

--- queries/book3chapter7.sql
+++ queries/book3chapter7.sql
@@ -47,3 +47,61 @@ INSERT INTO dealershipemployees
 SELECT d.dealership_id, employeeID1
 FROM dealerships d
 WHERE d.business_name = 'Meadley Autos of California';
+
+
+-- Create a transaction for:
+-- Creating a new dealership in Washington, D.C. called Felphun Automotive
+-- Hire 3 new employees for the new dealership: Sales Manager, General Manager and Customer Service.
+-- All employees that currently work at Nelsen Autos of Illinois will now start working at Cain Autos of Missouri instead.
+
+BEGIN;
+DECLARE
+	dealershipId int;
+	employeeID1 int;
+	employeeID2 int;
+	employeeID3 int;
+
+INSERT INTO dealerships
+(business_name, phone, city, state, website, tax_id)
+VALUES 'Felphun Automotive', '345-232-1342', 'Nashville', 'Tennessee', 'gh-856-a8d5'
+RETURNING dealership_id INTO dealershipId;
+
+INSERT INTO employees
+(first_name, last_name, email_address, phone, employee_type_id)
+SELECT 'Bob', 'Johnson', 'bob@johnson.com', '334-564-5555', et.employee_type_id
+FROM employeetypes et
+WHERE et.name = 'Sales Manager'
+RETURNING employee_id INTO employeeID1;
+
+INSERT INTO employees
+(first_name, last_name, email_address, phone, employee_type_id)
+SELECT 'Steve', 'Stevens', 'steve@stevens.com', '444-555-6666', et.employee_type_id
+FROM employeetypes et
+WHERE et.name = 'General Manager'
+RETURNING employee_id INTO employeeID2;
+
+INSERT INTO employees
+(first_name, last_name, email_address, phone, employee_type_id)
+SELECT 'Mark', 'Jacobs', 'mark@jacobs.com', '332-334-5643', et.employee_type_id
+FROM employeetypes et
+WHERE et.name = 'Customer Service'
+RETURNING employee_id INTO employeeID3;
+
+INSERT INTO dealershipemployees
+(dealership_id, employee_id)
+VALUES (dealershipId, employeeID1);
+
+INSERT INTO dealershipemployees
+(dealership_id, employee_id)
+VALUES (dealershipId, employeeID2);
+
+INSERT INTO dealershipemployees
+(dealership_id, employee_id)
+VALUES (dealershipId, employeeID3);
+
+UPDATE dealershipemployees
+SET dealership_id = (SELECT dealership_id FROM dealerships d WHERE d.business_name = 'Cain Autos of Missouri')
+WHERE dealership_id = (SELECT dealership_id FROM dealerships d WHERE d.business_name = 'Nelsen Autos of Illinois');
+
+COMMIT;
+
